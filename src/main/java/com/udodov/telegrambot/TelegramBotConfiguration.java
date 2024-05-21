bd59f7785140e8a01704b1e67e7e9c4818b94f18
SergeiUdodov <89786620+SergeiUdodov@users.noreply.github.com>
2024-05-22 02:33:43 +0300

    Добавлен класс и базовая конфигурация бота

---
+++ src/main/java/com/udodov/telegrambot/TelegramBotConfiguration.java
@@ -0,0 +1,28 @@
+package com.udodov.telegrambot;
+
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Configuration
+public class TelegramBotConfiguration {
+
+    @Bean
+    @SneakyThrows
+    public TelegramBot telegramBot(@Value("${bot.token}") String botToken, TelegramBotsApi telegramBotsApi) {
+        DefaultBotOptions botOptions = new DefaultBotOptions();
+        TelegramBot bot = new TelegramBot(botOptions, botToken);
+        telegramBotsApi.registerBot(bot);
+        return bot;
+    }
+
+    @Bean
+    @SneakyThrows
+    public TelegramBotsApi telegramBotsApi() {
+        return new TelegramBotsApi(DefaultBotSession.class);
+    }
+}
---
+++ src/test/java/com/udodov/telegrambot/TelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udodov.telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
